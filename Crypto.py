c07e7de93d76295fe1fd28c004d93d7f55a7707d
100120870 <condorvirus@gmail.com>
2019-10-29 15:45:42 -0500

    Decription

--- Crypto.py
+++ Crypto.py
@@ -13,3 +13,17 @@ def scrable2Encrypt(plainText):
         charCount = charCount + 1
     cipherText = oddChars + evenChars
     return cipherText
+
+def scramble2Decrypt(cipherText):
+    halfLength = len(cipherText) // 2
+    evenChars = cipherText[halfLength:] # half way to the end
+    oddChars = cipherText[:halfLength]  # start to half way
+    plainText = ""
+
+    for i in range(halfLength):
+        plainText = plainText + evenChars[i]
+        plainText = plainText + oddChars[i]
+
+        if len(oddChars) < len(evenChars):
+            plainText = plainText + evenChars[-1]
+    return plainText
